26012d45218f0239d62651a17e9e056eaebacce5
andreasHovaldt <andreas.hovaldt@gmail.com>
2024-12-01 03:05:57 +0100

    Small fix

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@ echo "Switching to branch MAIN"
 git checkout main
 
 echo "Pulling latest changes from MAIN"
-git pull origin main
+git pull
 
 echo "Building the project"
 npm run build
 
 echo "Copying the build to the server"
-scp -r build/* web-server:/var/www/dreez/
+scp -r dist/* web-server:/var/www/dreez/
 
 echo "Deployment completed successfully"
