ce60c6fecb9d489ab25a40e68eac993cc569060c
Andreas Hovaldt Højrup <andreas.hovaldt@gmail.com>
2025-11-04 23:18:18 +0100

    Small ssh server name change

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ echo "Building the project"
 npm run build
 
 echo "Copying the build to the server"
-scp -r dist/* web-server:/var/www/dreez/
+scp -r dist/* web-server-local:/var/www/dreez/
 
 echo "Deployment completed successfully"
+echo "NOTE: If the new deployment is not available on the website, run 'sudo systemctl reload nginx' on the web server" 
